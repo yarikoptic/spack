18282d66b370fd2108e26a9eaf6fbed62c321310
Gregory Becker <becker33@llnl.gov>
2022-12-12 11:54:28 -0800

    solver: remove indirection for external conditions

--- lib/spack/spack/solver/asp.py
+++ lib/spack/spack/solver/asp.py
@@ -1328,7 +1328,9 @@ class SpackSolverSetup(object):
             for local_idx, spec in enumerate(external_specs):
                 msg = "%s available as external when satisfying %s" % (spec.name, spec)
                 condition_id = self.condition(spec, msg=msg)
-                self.gen.fact(fn.possible_external(condition_id, pkg_name, local_idx))
+                self.gen.fact(fn.imposed_constraint(
+                    condition_id, "external_conditions_hold", pkg_name, local_idx
+                ))
                 self.possible_versions[spec.name].add(spec.version)
                 self.gen.newline()
 
@@ -2158,6 +2160,7 @@ class SpecBuilder(object):
                 r"^root$",
                 r"^virtual_node$",
                 r"^virtual_root$",
+                r"^.*holds?$",
             ]
         )
     )
